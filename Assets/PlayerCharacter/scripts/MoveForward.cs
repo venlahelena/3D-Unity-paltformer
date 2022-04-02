6c801b6f1354c2c92943654f1d3be49bc468c416
Venla Joikokko <t9jove00@students.oamk.fi>
2022-04-02 16:04:09 +0900

    Some changes and fixes to character controls

---
+++ Assets/PlayerCharacter/scripts/MoveForward.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameProgrammingLanguageTermProject
+{
+        [CreateAssetMenu(fileName = "New State", menuName = "AbilityData/MoveForward")]
+
+        public class MoveForward : StateData
+        {
+            public float Speed;
+
+            public override void UpdateAbility(CharacterStateBase characterStateBase, Animator animator)
+            {
+                //Get character control from state base.
+                CharacterControl c = characterStateBase.GetCharacterControl(animator);
+
+                //If we press A and D key at the same time character should not react
+                if (c.MoveRight && c.MoveLeft)
+                {
+                    //When character is not moving aka player is not pressing button to move animation should be played
+                    animator.SetBool(TransitionParameter.Walk.ToString(), false);
+                    return;
+                }
+
+                //When A or D movement buttons are not pressed walking naimtion should not be triggered
+                if (!c.MoveRight && !c.MoveLeft)
+                {
+                    animator.SetBool(TransitionParameter.Walk.ToString(), false);
+                    return;
+                }
+
+                //when player press D key character moves Right/forwards
+                if (c.MoveRight)
+                {
+                    c.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+                    c.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                }
+                //When player presses A key character moves left/backwards
+                if (c.MoveLeft)
+                {
+                    c.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+                    c.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+        }
+    }
+}
