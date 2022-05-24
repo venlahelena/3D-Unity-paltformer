72d9e8460b26cef4bd13726fb9a5fd96be99b7df
Venla Joikokko <t9jove00@students.oamk.fi>
2022-05-24 12:37:02 +0900

    First draft of the final game

--- Assets/PlayerCharacter/scripts/ItemCollector.cs
+++ Assets/PlayerCharacter/scripts/ItemCollector.cs
@@ -2,14 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ItemCollector : MonoBehaviour
 {
     private int apples = 0;
     private int mushrooms = 0;
 
+    public int applesToCollect = 0;
+    public int mushroomsToCollect = 0;
+
+    public GameObject enterText;
+    public GameObject enterTextNotComp;
+    public string levelToLoad;
+
     [SerializeField] private Text applesText;
     [SerializeField] private Text mushroomsText;
+
+    private void Start()
+    {
+        enterText.SetActive(false);
+        enterTextNotComp.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -28,5 +42,20 @@ public class ItemCollector : MonoBehaviour
             mushroomsText.text = "Mushrooms: " + mushrooms;
             
         }
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            if (apples == applesToCollect && mushrooms == mushroomsToCollect)
+            {
+                Invoke("OnTriggerEnter", 2.0f);
+                enterText.SetActive(true);
+                SceneManager.LoadScene(levelToLoad);
+            }
+            else
+            {
+                enterTextNotComp.SetActive(true);
+            }
+        }
     }
+
+    
 }
